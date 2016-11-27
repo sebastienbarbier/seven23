e12416158a9ff2c8d4f62c184a180c0dac7e4d5f
Sébastien BARBIER <contact@sebastienbarbier.com>
2016-11-27 10:36:30 +0700

    Implement About content with logo and link to personnal website

--- src/app/components/login/About.js
+++ src/app/components/login/About.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react';
 
-import {Card, CardText} from 'material-ui/Card';
+import {Card, CardText, CardTitle} from 'material-ui/Card';
+import Favorite from 'material-ui/svg-icons/action/favorite';
+import {red600} from 'material-ui/styles/colors';
+
+const styles = {
+  favorite: {
+    width: '20px',
+    position: 'relative',
+    top: '5px',
+    left: '4px',
+  },
+};
 
 class About extends Component {
 
@@ -13,7 +24,7 @@ class About extends Component {
     return (
       <Card>
         <CardText expandable={false}>
-
+          <p className="poweredby">Powered by <a href="https://sebastienbarbier.com" target="_blank"><img src="images/sebastienbarbier.logo.png" alt="Sébastien BARBIER" style={styles.logo} /></a> with <Favorite style={styles.favorite} color={red600} /></p>
         </CardText>
       </Card>
     );
