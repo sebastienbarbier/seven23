dc27d5b3df58d6dd4ba16f1b106b39bea27e9edf
Sébastien BARBIER <contact@sebastienbarbier.com>
2018-10-07 20:43:48 +0700

    First version of the signup form

--- src/app/components/login/SignUpForm.js
+++ src/app/components/login/SignUpForm.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 // import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 
 import TextField from '@material-ui/core/TextField';
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -12,44 +14,28 @@ import Button from '@material-ui/core/Button';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 
+import MobileStepper from '@material-ui/core/MobileStepper';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
 import UserActions from '../../actions/UserActions';
 
 import TermsAndConditionsDialog from '../legal/TermsAndConditionsDialog';
 
-const styles = {
-  actions: {
-    textAlign: 'right',
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
   },
-  floatLeft: {
-    float: 'left',
-    marginLeft: '10px',
+  form: {
+    flexGrow: 1,
   },
-  input: {
-    width: '100%',
-    display: 'block',
-  },
-  cardText: {
-    width: '100%',
-    margin: 'auto',
-    paddingTop: '0px',
-    paddingBottom: '32px',
-  },
-  checkbox: {
-    marginTop: '10px',
-    marginBottom: '6px',
-  },
-  termsandconditions: {
-    float: 'left',
-    marginLeft: '10px',
-  },
-  loading: {
-    textAlign: 'center',
-    padding: '50px 0',
-  },
-  error: {
-    color: 'red',
-  },
-};
+  mobileStepper: {
+    background: 'none'
+  }
+});
 
 class SignUpForm extends Component {
   constructor(props, context) {
@@ -65,6 +51,8 @@ class SignUpForm extends Component {
       loading: false,
       open: false,
       error: {},
+      activeStep: 0,
+      maxSteps: 4,
     };
   }
 
@@ -105,6 +93,18 @@ class SignUpForm extends Component {
   handleOpen = () => {
     this.setState({
       open: true,
+    });
+  };
+
+  handleNext = () => {
+    this.setState({
+      activeStep: this.state.activeStep + 1,
+    });
+  };
+
+  handleBack = () => {
+    this.setState({
+      activeStep: this.state.activeStep - 1,
     });
   };
 
@@ -153,8 +153,10 @@ class SignUpForm extends Component {
   };
 
   render() {
+    const { classes, theme } = this.props;
+    const { activeStep, maxSteps } = this.state;
     return (
-      <div>
+      <div className={classes.root}>
         {this.state.loading ? (
           <div className="flexboxContainer">
             <div className="flexbox">
@@ -162,88 +164,93 @@ class SignUpForm extends Component {
             </div>
           </div>
         ) : (
-          <form onSubmit={this.handleSubmit}>
-            <div>
-              <h2 style={{ fontSize: '2.3em' }}>Sign up</h2>
-              <div style={styles.cardText}>
-                {this.state.loading ? (
-                  <div style={styles.loading}>
-                    <CircularProgress size={80} />
-                  </div>
-                ) : (
-                  <div>
-                    <TextField
-                      label="Username"
-                      style={styles.input}
-                      value={this.state.username}
-                      error={Boolean(this.state.error.username)}
-                      helperText={this.state.error.username}
-                      onChange={this.handleChangeUsername}
-                      autoFocus={true}
-                      margin="normal"
-                      fullWidth
-                    />
-                    <TextField
-                      label="Email"
-                      style={styles.input}
-                      value={this.state.email}
-                      error={Boolean(this.state.error.email)}
-                      helperText={this.state.error.email}
-                      onChange={this.handleChangeEmail}
-                      margin="normal"
-                      fullWidth
-                    />
-                    <TextField
-                      label="Password"
-                      type="password"
-                      style={styles.input}
-                      value={this.state.password1}
-                      error={Boolean(this.state.error.password1)}
-                      helperText={this.state.error.password1}
-                      onChange={this.handleChangePassword}
-                      margin="normal"
-                      fullWidth
-                    />
-                    <TextField
-                      label="Repeat password"
-                      type="password"
-                      style={styles.input}
-                      value={this.state.password2}
-                      error={Boolean(this.state.error.password2)}
-                      helperText={this.state.error.password2}
-                      onChange={this.handleChangeRepeatPassword}
-                      margin="normal"
-                      fullWidth
-                    />
-                    <br />
-                    {this.state.error.termsandconditions ? (
-                      <p style={styles.error}>
-                        {this.state.error.termsandconditions}
-                      </p>
+          <div className={classes.form}>
+            { activeStep === 0 ? (
+              <form onSubmit={this.handleSubmit}>
+                <div>
+                  <h2 style={{ fontSize: '2.3em' }}>Sign up</h2>
+                  <div style={styles.cardText}>
+                    {this.state.loading ? (
+                      <div style={styles.loading}>
+                        <CircularProgress size={80} />
+                      </div>
                     ) : (
-                      ''
-                    )}
-
-                    <FormControlLabel
-                      control={
-                        <Checkbox
-                          name="agreed"
-                          onChange={this.handleCheck}
-                          style={styles.checkbox}
+                      <div>
+                        <TextField
+                          label="Username"
+                          style={styles.input}
+                          value={this.state.username}
+                          error={Boolean(this.state.error.username)}
+                          helperText={this.state.error.username}
+                          onChange={this.handleChangeUsername}
+                          autoFocus={true}
+                          margin="normal"
+                          fullWidth
                         />
-                      }
-                      label="I have read and agree with terms and conditions"
-                    />
+                        <TextField
+                          label="Email"
+                          style={styles.input}
+                          value={this.state.email}
+                          error={Boolean(this.state.error.email)}
+                          helperText={this.state.error.email}
+                          onChange={this.handleChangeEmail}
+                          margin="normal"
+                          fullWidth
+                        />
+                        <TextField
+                          label="Password"
+                          type="password"
+                          style={styles.input}
+                          value={this.state.password1}
+                          error={Boolean(this.state.error.password1)}
+                          helperText={this.state.error.password1}
+                          onChange={this.handleChangePassword}
+                          margin="normal"
+                          fullWidth
+                        />
+                        <TextField
+                          label="Repeat password"
+                          type="password"
+                          style={styles.input}
+                          value={this.state.password2}
+                          error={Boolean(this.state.error.password2)}
+                          helperText={this.state.error.password2}
+                          onChange={this.handleChangeRepeatPassword}
+                          margin="normal"
+                          fullWidth
+                        />
+                        <br />
+                        {this.state.error.termsandconditions ? (
+                          <p style={styles.error}>
+                            {this.state.error.termsandconditions}
+                          </p>
+                        ) : (
+                          ''
+                        )}
+                      </div>
+                    )}
                   </div>
-                )}
-              </div>
+
+                </div>
+
+              </form>
+            ) : ''}
+            { activeStep === 1 ? (
               <div style={styles.actions}>
+
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      name="agreed"
+                      onChange={this.handleCheck}
+                      style={styles.checkbox}
+                    />
+                  }
+                  label="I have read and agree with terms and conditions"
+                />
                 <Button onClick={this.handleOpen}>
                   Terms and conditions
                 </Button>
-                <Link to="/login">
-                  <Button>Cancel</Button>
-                </Link>
                 {this.state.loading ? (
                   <CircularProgress size={20} style={styles.loading} />
                 ) : (
@@ -251,22 +258,56 @@ class SignUpForm extends Component {
                     Sign up
                   </Button>
                 )}
-              </div>
-            </div>
 
-            <TermsAndConditionsDialog open={Boolean(this.state.open)} />
-          </form>
+                <TermsAndConditionsDialog open={Boolean(this.state.open)} />
+              </div>
+            ) : ''}
+            { activeStep === 2 ? (
+              <p>End to end encryption, backup key.</p>
+            ) : ''}
+            { activeStep === 3 ? (
+              <p>Confirm your email</p>
+            ) : ''}
+          </div>
         )}
+        <MobileStepper
+          steps={maxSteps}
+          position="static"
+          activeStep={activeStep}
+          className={classes.mobileStepper}
+          nextButton={
+            <Button size="small" onClick={this.handleNext} disabled={activeStep === maxSteps - 1}>
+              Next
+              {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+            </Button>
+          }
+          backButton={
+            activeStep != 0 ? (
+              <Button size="small" onClick={this.handleBack} disabled={activeStep === 0}>
+                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+                Back
+              </Button>
+            ) : (
+              <Link to="/login">
+                <Button size="small">
+                  Cancel
+                </Button>
+              </Link>
+            )
+          }
+        />
       </div>
     );
   }
 }
 
 SignUpForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state, ownProps) => {
   return {};
 };
 
-export default connect(mapStateToProps)(SignUpForm);
+export default withStyles(styles, { withTheme: true })(connect(mapStateToProps)(SignUpForm));
