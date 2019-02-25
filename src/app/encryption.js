96a97f42bd9c1c2586ab03e3392c766bd680e6bc
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-02-25 14:16:36 +0700

    Fix crypto to use not 256bits key instead of 192 not working on Chrome

--- src/app/encryption.js
+++ src/app/encryption.js
@@ -2,13 +2,37 @@
 
 import { Jose, JoseJWE } from 'jose-jwe-jws';
 
+function arrayFromString(str)  {
+  var arr = str.split('').map(function(c) {
+    return c.charCodeAt(0);
+  });
+  return new Uint8Array(arr);
+};
+
+function b64tob64u(a){
+    a=a.replace(/\=/g,"");
+    a=a.replace(/\+/g,"-");
+    a=a.replace(/\//g,"_");
+    return a
+}
+
+function _arrayBufferToBase64( buffer ) {
+    var binary = '';
+    var bytes = new Uint8Array( buffer );
+    var len = bytes.byteLength;
+    for (var i = 0; i < len; i++) {
+        binary += String.fromCharCode( bytes[ i ] );
+    }
+    return b64tob64u(btoa( binary ));
+}
+
 const ERROR_NO_KEY = 'Encryption Key missing. Please use Encryption.key(input) before processing data.';
 export class Encryption {
 
   constructor() {
 
     this.cryptographer = new Jose.WebCryptographer();
-    this.cryptographer.setKeyEncryptionAlgorithm("A128KW");
+    this.cryptographer.setKeyEncryptionAlgorithm("A256KW");
     this.cryptographer.setContentEncryptionAlgorithm("A128CBC-HS256");
 
     this.encrypter = null;
@@ -19,11 +43,24 @@ export class Encryption {
 
   key = (key) => {
     const that = this;
-    return new Promise((resolve) => {
-      that._key = Jose.crypto.subtle.importKey("jwk", {"kty":"oct", "k": key}, {name: "AES-KW"}, true, ["wrapKey", "unwrapKey"]);
-      that.encrypter = new JoseJWE.Encrypter(that.cryptographer, that._key);
-      that.decrypter = new JoseJWE.Decrypter(that.cryptographer, that._key);
-      resolve();
+    return new Promise((resolve, reject) => {
+      Jose.crypto.subtle.digest({name: "SHA-256"}, arrayFromString(key)).then(function(hash) {
+
+        that._key = Jose.crypto.subtle.importKey(
+          "jwk",
+          {"kty":"oct", "k": _arrayBufferToBase64(hash), "length": 256, "alg": "A256KW"},
+          {name: "AES-KW"},
+          true,
+          ["wrapKey", "unwrapKey"]
+        );
+
+        that._key.then(_ => {
+          that.encrypter = new JoseJWE.Encrypter(that.cryptographer, that._key);
+          that.decrypter = new JoseJWE.Decrypter(that.cryptographer, that._key);
+          resolve();
+        }).catch(reject);
+      });
+
     });
   };
 
