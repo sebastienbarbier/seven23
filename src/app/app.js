feee0ec09f404635936b8df9344559ddb32e82e9
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-03-05 14:48:26 +0700

    Add workbox-webpack-plugin to cache app locally

--- src/app/app.js
+++ src/app/app.js
@@ -32,6 +32,16 @@ import '../www/styles/index.scss';
   throttle('resize', 'optimizedResize');
 })();
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      // console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      // console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
 render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
--- webpack-production.config.js
+++ webpack-production.config.js
@@ -4,6 +4,7 @@ const buildPath = path.resolve(__dirname, 'build');
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
 const TransferWebpackPlugin = require('transfer-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 const config = {
   entry: [
@@ -31,7 +32,13 @@ const config = {
       {from: 'www/html'},
       {from: 'www/config'},
       {from: 'www/images', to: 'images'},
-    ], path.resolve(__dirname, 'src'))
+    ], path.resolve(__dirname, 'src')),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling 'old' SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true
+    })
   ],
   module: {
     rules: [
