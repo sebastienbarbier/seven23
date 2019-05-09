5357f9006dbcf2c0d830529d465dfe1a43d0f362
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-05-09 15:57:28 +0700

    Refactor workers with better code base

--- src/app/actions/AppActions.js
+++ src/app/actions/AppActions.js
@@ -3,7 +3,8 @@ import {
 } from '../constants';
 
 var ReportActions = {
-
+  /* Navigate event save current url to reopen the app as if the user never left
+  (was really usefull on smartphone before iOS kept webapp states on leave event).  */
   navigate: (url) => {
     return {
       type: NAVIGATE,
--- src/app/workers/Transactions.worker.js
+++ src/app/workers/Transactions.worker.js
@@ -17,7 +17,8 @@ import axios from 'axios';
 import storage from '../storage';
 import encryption from '../encryption';
 
-var firstRating = {};
+import { firstRating, getChangeChain } from './utils/changeChain';
+
 var cachedChain = null;
 var last_edited = null;
 
@@ -190,7 +191,7 @@ onmessage = function(event) {
             const day = response_transaction.date.slice(8, 10);
             response_transaction.date = new Date(Date.UTC(year, month - 1, day, 0, 0, 0));
 
-            getChangeChain(response.data.account).then(chain => {
+            getCachedChangeChain(response.data.account).then(chain => {
               // Connect to indexedDB
               let connectDB = indexedDB.open(DB_NAME, DB_VERSION);
               connectDB.onsuccess = function(event) {
@@ -650,7 +651,7 @@ function retrieveTransactions(account, currency) {
           let promises = [];
           let counter = 0;
 
-          getChangeChain(account).then(chain => {
+          getCachedChangeChain(account).then(chain => {
             transactions.forEach(transaction => {
               transaction.date = new Date(transaction.date);
               if (transaction.date < youngest) {
@@ -689,8 +690,10 @@ function convertTo(transaction, currencyId, accountId) {
         transaction.amount = transaction.originalAmount;
         resolve();
       } else {
-        getChangeChain(accountId)
+        getCachedChangeChain(accountId)
           .then(chain => {
+            console.log(chain);
+            console.log(firstRating);
             const result = chain.find(item => {
               return item.date <= transaction.date;
             });
@@ -756,163 +759,15 @@ function convertTo(transaction, currencyId, accountId) {
 }
 
 // Duplicate in Change worker
-function getChangeChain(accountId) {
+function getCachedChangeChain(accountId) {
   return new Promise((resolve, reject) => {
-    var chain = [];
-    var lastItem = {};
-    var changes = [];
-
     if (cachedChain) {
       resolve(cachedChain);
     } else {
-      let connectDB = indexedDB.open(DB_NAME, DB_VERSION);
-      connectDB.onsuccess = function(event) {
-        var index = event.target.result
-          .transaction('changes')
-          .objectStore('changes')
-          .index('account');
-
-        var keyRange = IDBKeyRange.only(parseInt(accountId));
-        let cursor = index.openCursor(keyRange);
-
-        cursor.onsuccess = function(event) {
-          var cursor = event.target.result;
-          if (cursor) {
-            let change = event.target.result.value;
-            change.exchange_rate = parseFloat(change.new_amount) / parseFloat(change.local_amount);
-            changes.push(change);
-            cursor.continue();
-          } else {
-            changes = changes.sort((a, b) => {
-              return a.date > b.date ? 1 : -1;
-            });
-
-            for (var i in changes) {
-              var item = {
-                id: changes[i].id,
-                account: changes[i].account,
-                date: new Date(changes[i].date),
-                rates: Object.assign({}, lastItem.rates),
-                secondDegree: Object.assign({}, lastItem.secondDegree),
-              };
-
-              // GENERATE FIRST RATING
-              // If first time using this localCurrency
-              if (item.rates[changes[i]['local_currency']] === undefined) {
-                firstRating[changes[i]['local_currency']] = {};
-              }
-              if (
-                firstRating[changes[i]['local_currency']][changes[i]['new_currency']] === undefined
-              ) {
-                firstRating[changes[i]['local_currency']][changes[i]['new_currency']] = changes[i]['exchange_rate'];
-              }
-
-              // If first time using this new Currency
-              if (item.rates[changes[i]['new_currency']] === undefined) {
-                firstRating[changes[i]['new_currency']] = {};
-              }
-              if (
-                firstRating[changes[i]['new_currency']][changes[i]['local_currency']] === undefined
-              ) {
-                firstRating[changes[i]['new_currency']][changes[i]['local_currency']] =
-                  1 / changes[i]['exchange_rate'];
-              }
-
-              // GENERERATE CHAIN ITEM
-              item.rates[changes[i]['local_currency']] =
-                Object.assign({}, item.rates[changes[i]['local_currency']]);
-              item.rates[changes[i]['local_currency']][changes[i]['new_currency']] = changes[i]['exchange_rate'];
-
-              item.rates[changes[i]['new_currency']] =
-                Object.assign({}, item.rates[changes[i]['new_currency']]);
-              item.rates[changes[i]['new_currency']][changes[i]['local_currency']] =
-                  1 / changes[i]['exchange_rate'];
-
-              // CALCULATE CROSS REFERENCE RATE WITH MULTI CURRENCY VALUES
-              //
-              // console.log('rates');
-              // console.log(JSON.stringify(item.rates));
-              // We take
-              //
-              // Algo:
-              //  1 -> 2 -> 3
-              //    x    y
-              //  1 is changes[i]['local_currency']
-              //  2 is changes[i]['new_currency']
-              //  x is exchange rate between 1 and 2
-              //  we need to calculate y and save it as 1 -> 3
-              Object.keys(item.rates[changes[i]['local_currency']])
-                .forEach((key) => {
-                  const value = item.rates[changes[i]['local_currency']][key];
-                  if (key !== changes[i]['new_currency']) {
-                    item.rates[key];
-                    // console.log('local to key');
-                    // console.log(changes[i]['local_currency'] + ' > ' + key + ' > ' + changes[i]['new_currency'] );
-                    // console.log(changes[i]['local_currency'] + ' > ' + changes[i]['new_currency'] + ' : ' + changes[i]['exchange_rate'] );
-                    // console.log(changes[i]['local_currency'] + ' > ' + key + ' : ' + item.rates.get(changes[i]['local_currency']).get(key) );
-                    // console.log(key + ' > ' + changes[i]['new_currency'] + ' : ' + changes[i]['exchange_rate'] / value );
-                    // console.log(changes[i]['new_currency'] + ' > ' + key + ' : ' + 1/(changes[i]['exchange_rate'] / value));
-
-                    if (item.secondDegree[key] === undefined) {
-                      item.secondDegree[key] = {};
-                    }
-                    item.secondDegree[key][changes[i]['new_currency']] =
-                        changes[i]['exchange_rate'] / value;
-
-                    if (
-                      item.secondDegree[changes[i]['new_currency']] ===
-                      undefined
-                    ) {
-                      item.secondDegree[changes[i]['new_currency']] = {};
-                    }
-                    item.secondDegree[changes[i]['new_currency']][key] =
-                      1 / (changes[i]['exchange_rate'] / value);
-
-                    // We also need to update firstRate with this new value ... sad :(
-                    if (firstRating[key] === undefined) {
-                      firstRating[key] = {};
-                    }
-                    if (
-                      firstRating[key][changes[i]['new_currency']] ===
-                      undefined
-                    ) {
-                      firstRating[key][changes[i]['new_currency']] =
-                        changes[i]['exchange_rate'] / value;
-                    }
-
-                    if (
-                      firstRating[changes[i]['new_currency']] === undefined
-                    ) {
-                      firstRating[changes[i]['new_currency']] = {};
-                    }
-                    if (
-                      firstRating[changes[i]['new_currency']][key] === undefined
-                    ) {
-                      firstRating[changes[i]['new_currency']][key] =
-                        1 / (changes[i]['exchange_rate'] / value);
-                    }
-
-                    // console.log('secondDegree');
-                    // console.log(JSON.stringify(item.secondDegree));
-                  }
-                  // item.secondDegree = item.secondDegree;
-                });
-
-              chain.push(item);
-              lastItem = item;
-            }
-
-            chain = chain.sort((a, b) => {
-              return a.date < b.date ? 1 : -1;
-            });
-            cachedChain = chain;
-            resolve(chain);
-          }
-        };
-      }; // end connectDB.onsuccess
-      connectDB.onerror = function(event) {
-        console.error(event);
-      };
+      getChangeChain(accountId).then(chain => {
+        cachedChain = chain;
+        resolve(chain);
+      });
     }
   });
 }
