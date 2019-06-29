897b3410dbec3bdf514f5f5354ef74f65a0c5c1e
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-06-29 18:10:14 +0800

    Fix bug on category view with wrong category id used

--- src/app/components/categories/Category.js
+++ src/app/components/categories/Category.js
@@ -22,7 +22,7 @@ export function Category(props) {
   const [transactions, setTransactions] = useState(null);
 
   function performSearch() {
-    dispatch(StatisticsActions.perCategory(category.id))
+    dispatch(StatisticsActions.perCategory(props.category.id))
       .then(args => {
         if (args && args.transactions && Array.isArray(args.transactions)) {
           setTransactions(args.transactions);
@@ -67,7 +67,7 @@ export function Category(props) {
         </Button>
       </div>
 
-      <div style={{ paddingBottom: 20 }}>
+      <div style={{ paddingBottom: 20, margin: "8px 20px" }}>
         {transactions && transactions.length === 0 ? (
           <p>You have no transaction</p>
         ) : (
@@ -123,164 +123,3 @@ export function Category(props) {
     </div>
   );
 }
-
-// class Category extends Component {
-//   constructor(props, context) {
-//     super(props, context);
-//     this.history = props.history;
-//     this.state = {
-//       category: props.category,
-//       categories: props.categories,
-//       onEditCategory: props.onEditCategory,
-//       onEditTransaction: props.onEditTransaction,
-//       onDuplicationTransaction: props.onDuplicationTransaction,
-//       transactions: null,
-//       stats: null,
-//       graph: [],
-//       loading: true,
-//       snackbar: {
-//         open: false,
-//         message: ""
-//       }
-//     };
-//     this.context = context;
-//   }
-
-//   _processData = (category = this.state.category) => {
-//     const { dispatch } = this.props;
-
-//     dispatch(StatisticsActions.perCategory(category.id))
-//       .then(args => {
-//         if (args && args.transactions && Array.isArray(args.transactions)) {
-//           // Generate Graph data
-//           let lineExpenses = {
-//             // color: 'red',
-//             values: []
-//           };
-
-//           let lineIncomes = {
-//             values: []
-//           };
-
-//           Object.keys(args.stats.perDates).forEach(year => {
-//             // For each month of year
-//             Object.keys(args.stats.perDates[year].months).forEach(month => {
-//               if (args.stats.perDates[year].months[month]) {
-//                 lineExpenses.values.push({
-//                   date: new Date(year, month),
-//                   value: +args.stats.perDates[year].months[month].expenses * -1
-//                 });
-//                 lineIncomes.values.push({
-//                   date: new Date(year, month),
-//                   value: args.stats.perDates[year].months[month].incomes
-//                 });
-//               } else {
-//                 lineExpenses.values.push({
-//                   date: new Date(year, month),
-//                   value: 0
-//                 });
-//                 lineIncomes.values.push({
-//                   date: new Date(year, month),
-//                   value: 0
-//                 });
-//               }
-//             });
-//           });
-
-//           this.setState({
-//             loading: false,
-//             stats: args.stats,
-//             graph: [lineExpenses], // lineIncomes
-//             transactions: args.transactions
-//           });
-//         }
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   };
-
-//   handleDeleteCategory = (selectedCategory = {}) => {
-//     this.history.push("/categories/");
-
-//     const { dispatch } = this.props;
-//     dispatch(CategoryActions.delete(selectedCategory.id)).then(() => {
-//       this.setState({
-//         snackbar: {
-//           open: true,
-//           message: "Deleted with success",
-//           deletedItem: selectedCategory
-//         }
-//       });
-//     });
-//   };
-
-//   componentDidMount() {
-//     if (this.state.category) {
-//       this._processData();
-//     }
-//   }
-
-//   componentWillReceiveProps(newProps) {
-//     if (this.props.account.id !== newProps.account.id) {
-//       this.history.push("/categories");
-//     }
-
-//     if (newProps.category && newProps.category.id ) {
-//       this.setState({
-//         category: newProps.category,
-//         categories: newProps.categories,
-//         onEditTransaction: newProps.onEditTransaction,
-//         onDuplicationTransaction: newProps.onDuplicationTransaction,
-//         transactions: null,
-//         stats: null,
-//         open: false,
-//         loading: true
-//       });
-
-//       this._processData(newProps.category);
-//     }
-//   }
-
-//   _openActionMenu = event => {
-//     this.setState({
-//       anchorEl: event.currentTarget
-//     });
-//   };
-
-//   _closeActionMenu = () => {
-//     this.setState({
-//       anchorEl: null
-//     });
-//   };
-
-//   render() {
-//     const { anchorEl, category } = this.state;
-//     const { categories, isLoading } = this.props;
-
-//     return (
-//
-//     );
-//   }
-// }
-
-// Category.propTypes = {
-//   theme: PropTypes.object.isRequired,
-//   account: PropTypes.object.isRequired,
-//   transactions: PropTypes.array.isRequired,
-//   selectedCurrency: PropTypes.object.isRequired,
-//   categories: PropTypes.array.isRequired
-// };
-
-// const mapStateToProps = (state, ownProps) => {
-//   return {
-//     account: state.account,
-//     transactions: state.transactions,
-//     selectedCurrency: state.currencies.find(
-//       c => c.id === state.account.currency
-//     ),
-//     categories: state.categories.list
-//   };
-// };
-
-// export default connect(mapStateToProps)(withTheme(Category));
