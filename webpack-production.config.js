e4151108a7e68df8318fa6a6659f43ffbedddfec
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-07-23 17:24:10 +0700

    Configure Workbox to load NetworkFirst

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -1,71 +1,89 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const webpack = require('webpack');
-const path = require('path');
-const buildPath = path.resolve(__dirname, 'build');
-const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const TransferWebpackPlugin = require('transfer-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
-
+const webpack = require("webpack");
+const path = require("path");
+const buildPath = path.resolve(__dirname, "build");
+const nodeModulesPath = path.resolve(__dirname, "node_modules");
+const TransferWebpackPlugin = require("transfer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 const config = {
-  entry: [
-    path.join(__dirname, '/src/app/app.js')
-  ],
+  entry: [path.join(__dirname, "/src/app/app.js")],
   // Render source-map file for final build
-  devtool: 'cheap-eval-source-map',
+  devtool: "cheap-eval-source-map",
   // output config
   output: {
     path: buildPath, // Path of output file
-    filename: 'app.js', // Name of output file
+    filename: "app.js" // Name of output file
   },
   plugins: [
     new CleanWebpackPlugin(),
     // Define production build to allow React to strip out unnecessary checks
     new webpack.DefinePlugin({
-      'process.env':{
-        'NODE_ENV': JSON.stringify('production'),
-        'SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
-        'BUILD_DATE': JSON.stringify(new Date()),
-        'TRAVIS_COMMIT': JSON.stringify(process.env.TRAVIS_COMMIT),
+      "process.env": {
+        NODE_ENV: JSON.stringify("production"),
+        SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN),
+        BUILD_DATE: JSON.stringify(new Date()),
+        TRAVIS_COMMIT: JSON.stringify(process.env.TRAVIS_COMMIT)
       }
     }),
     // Allows error warnings but does not stop compiling.
     new webpack.NoEmitOnErrorsPlugin(),
     // Transfer Files
-    new TransferWebpackPlugin([
-      {from: 'www/html'},
-      {from: 'www/config'},
-      {from: 'www/images', to: 'images'},
-    ], path.resolve(__dirname, 'src')),
+    new TransferWebpackPlugin(
+      [
+        { from: "www/html" },
+        { from: "www/config" },
+        { from: "www/images", to: "images" }
+      ],
+      path.resolve(__dirname, "src")
+    ),
     new WorkboxPlugin.GenerateSW({
       // these options encourage the ServiceWorkers to get in there fast
       // and not allow any straggling 'old' SWs to hang around
       clientsClaim: true,
       skipWaiting: true,
-      include: [/\.html$/, /\.js$/, /\.jpg$/, /\.svg$/, /\.png$/, /\.json$/, /\.xml$/]
+      include: [
+        /\.html$/,
+        /\.js$/,
+        /\.jpg$/,
+        /\.svg$/,
+        /\.png$/,
+        /\.json$/,
+        /\.xml$/
+      ],
+      runtimeCaching: [
+        {
+          urlPattern: /./,
+          handler: "NetworkFirst"
+        }
+      ]
     })
   ],
   module: {
     rules: [
       {
         test: /\.js$/, // All .js files
-        use: ['babel-loader?presets[]=env&presets[]=react&presets[]=stage-2&sourceMaps=true&plugins[]=syntax-async-functions'], // react-hot is like browser sync and babel loads jsx and es6-7
-        exclude: [nodeModulesPath],
+        use: [
+          "babel-loader?presets[]=env&presets[]=react&presets[]=stage-2&sourceMaps=true&plugins[]=syntax-async-functions"
+        ], // react-hot is like browser sync and babel loads jsx and es6-7
+        exclude: [nodeModulesPath]
       },
       {
         test: /\.worker.js$/,
-        loader: 'worker-loader?inline&fallback=false'
+        loader: "worker-loader?inline&fallback=false"
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ["style-loader", "css-loader", "sass-loader"]
       },
-      { test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/,
-        loader: 'file-loader?name=[name].[ext]' },
-    ],
-  },
+      {
+        test: /\.(jpe?g|png|gif|svg|eot|woff|ttf|svg|woff2)$/,
+        loader: "file-loader?name=[name].[ext]"
+      }
+    ]
+  }
 };
 
 module.exports = config;
