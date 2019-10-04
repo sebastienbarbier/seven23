ac67e8a53a7e3cc91068635a0f132ede9bc8912a
Sébastien BARBIER <contact@sebastienbarbier.com>
2019-10-04 23:59:31 +0700

    Migrate settings to react hooks

--- src/app/components/Settings.js
+++ src/app/components/Settings.js
@@ -2,10 +2,12 @@
  * In this file, we create a React component
  * which incorporates components provided by Material-UI.
  */
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { useRouter } from "../router";
 
 import Card from "@material-ui/core/Card";
 
@@ -46,270 +48,232 @@ import SocialNetworksSettings from "./settings/SocialNetworksSettings";
 
 import UserButton from "./settings/UserButton";
 
-class Settings extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.history = props.history;
-    this.component = null;
+export default function Settings() {
+  const { history } = useRouter();
 
-    this.SETTINGS = {
-      PROFILE: {
-        title: "User profile",
-        url: "/settings/profile/",
-        subtitle: "Configure your data",
-        icon: <AccountBoxIcon />,
-        component: (
-          <ProfileSettings
-            onModal={component =>
-              component
-                ? this.modal(component)
-                : this.setState({ open: false, component: null })
-            }
-            history={this.history}
-          />
-        )
-      },
-      ACCOUNTS: {
-        title: "Accounts",
-        url: "/settings/accounts/",
-        subtitle: "Manage yours accounts",
-        icon: <AvLibraryBooks />,
-        component: (
-          <AccountsSettings
-            onModal={component =>
-              component
-                ? this.modal(component)
-                : this.setState({ open: false, component: null })
-            }
-          />
-        )
-      },
-      CURRENCIES: {
-        title: "Currencies",
-        url: "/settings/currencies/",
-        subtitle: "Select currencies to show",
-        icon: <MoneyIcon />,
-        component: <CurrenciesSettings />
-      },
-      SERVER: {
-        title: "Server",
-        url: "/settings/server/",
-        subtitle: "Details about your hosting",
-        icon: <StorageIcon />,
-        component: <ServerSettings history={this.history} />
-      },
-      SECURITY: {
-        title: "Security",
-        url: "/settings/security/",
-        subtitle: "Encryption key",
-        icon: <Lock />,
-        component: <SecuritySettings />
-      },
-      SUBSCRIPTION: {
-        title: "Subscription",
-        url: "/settings/subscription/",
-        subtitle: "Paiement, invoice, and extend",
-        icon: <CreditCard />,
-        component: <SubscriptionSettings history={this.history} />
-      },
-      IMPORT_EXPORT: {
-        title: "Import / Export",
-        url: "/settings/import/export/",
-        subtitle: "Backup and restore your data",
-        icon: <ImportExport />,
-        component: <ImportExportSettings />
-      },
-      SOCIAL_NETWORKS: {
-        title: "Social networks",
-        url: "/settings/social/",
-        subtitle: "Connect your accounts",
-        icon: <AccountTreeIcon />,
-        component: (
-          <SocialNetworksSettings
-            onModal={component =>
-              component
-                ? this.modal(component)
-                : this.setState({ open: false, component: null })
-            }
-          />
-        )
-      },
-      THEME: {
-        title: "Theme",
-        url: "/settings/theme/",
-        subtitle: "Light or dark mode",
-        icon: <StyleIcon />,
-        component: <ThemeSettings />
-      },
-      APP: {
-        title: "About the App",
-        url: "/settings/application/",
-        subtitle: "Version, force refresh",
-        icon: <SettingsApplications />,
-        component: <AppSettings />
-      },
-      HELP: {
-        title: "Help / Support",
-        url: "/settings/help/",
-        subtitle: "Bug report, questions, or anything else",
-        icon: <HelpIcon />,
-        component: <HelpSettings />
-      }
-    };
-
-    const page = this.SETTINGS[
-      Object.keys(this.SETTINGS).find(
-        key => this.SETTINGS[key].url === props.history.location.pathname
+  const SETTINGS = {
+    PROFILE: {
+      title: "User profile",
+      url: "/settings/profile/",
+      subtitle: "Configure your data",
+      icon: <AccountBoxIcon />,
+      component: (
+        <ProfileSettings
+          onModal={component => (component ? modal(component) : setOpen(false))}
+          history={history}
+        />
       )
-    ];
-
-    this.state = {
-      open: false,
-      page_title: page ? page.title : "",
-      page
-    };
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    const page = this.SETTINGS[
-      Object.keys(this.SETTINGS).find(
-        key => this.SETTINGS[key].url === nextProps.history.location.pathname
+    },
+    ACCOUNTS: {
+      title: "Accounts",
+      url: "/settings/accounts/",
+      subtitle: "Manage yours accounts",
+      icon: <AvLibraryBooks />,
+      component: (
+        <AccountsSettings
+          onModal={component => (component ? modal(component) : setOpen(false))}
+        />
       )
-    ];
+    },
+    CURRENCIES: {
+      title: "Currencies",
+      url: "/settings/currencies/",
+      subtitle: "Select currencies to show",
+      icon: <MoneyIcon />,
+      component: <CurrenciesSettings />
+    },
+    SERVER: {
+      title: "Server",
+      url: "/settings/server/",
+      subtitle: "Details about your hosting",
+      icon: <StorageIcon />,
+      component: <ServerSettings history={history} />
+    },
+    SECURITY: {
+      title: "Security",
+      url: "/settings/security/",
+      subtitle: "Encryption key",
+      icon: <Lock />,
+      component: <SecuritySettings />
+    },
+    SUBSCRIPTION: {
+      title: "Subscription",
+      url: "/settings/subscription/",
+      subtitle: "Paiement, invoice, and extend",
+      icon: <CreditCard />,
+      component: <SubscriptionSettings history={history} />
+    },
+    IMPORT_EXPORT: {
+      title: "Import / Export",
+      url: "/settings/import/export/",
+      subtitle: "Backup and restore your data",
+      icon: <ImportExport />,
+      component: <ImportExportSettings />
+    },
+    SOCIAL_NETWORKS: {
+      title: "Social networks",
+      url: "/settings/social/",
+      subtitle: "Connect your accounts",
+      icon: <AccountTreeIcon />,
+      component: (
+        <SocialNetworksSettings
+          onModal={component => (component ? modal(component) : setOpen(false))}
+        />
+      )
+    },
+    THEME: {
+      title: "Theme",
+      url: "/settings/theme/",
+      subtitle: "Light or dark mode",
+      icon: <StyleIcon />,
+      component: <ThemeSettings />
+    },
+    APP: {
+      title: "About the App",
+      url: "/settings/application/",
+      subtitle: "Version, force refresh",
+      icon: <SettingsApplications />,
+      component: <AppSettings />
+    },
+    HELP: {
+      title: "Help / Support",
+      url: "/settings/help/",
+      subtitle: "Bug report, questions, or anything else",
+      icon: <HelpIcon />,
+      component: <HelpSettings />
+    }
+  };
 
-    this.setState({
-      page_title: page ? page.title : this.state.page_title,
-      page
-    });
-  }
+  const server = useSelector(state => state.server);
 
-  modal(component) {
-    this.component = component;
-    this.setState({
-      open: true
-    });
-  }
+  let component = null;
 
-  drawListItem(page) {
+  const [open, setOpen] = useState(false);
+  const [page, setPage] = useState(
+    SETTINGS[
+      Object.keys(SETTINGS).find(
+        key => SETTINGS[key].url === history.location.pathname
+      )
+    ]
+  );
+  const [pageTitle, setPageTitle] = useState(page ? page.title : "");
+
+  useEffect(() => {
+    if (page) {
+      setPageTitle(page.title);
+    }
+  }, [page]);
+
+  const modal = component => {
+    component = component;
+    setOpen(true);
+  };
+
+  const drawListItem = _page => {
     return (
       <ListItem
         button
         onClick={(event, index) => {
-          this.setState({ page: page.url });
-          this.history.push(page.url);
+          setPage(_page);
+          history.push(_page.url);
         }}
-        selected={this.state.page === page}
+        selected={page === _page}
       >
-        <ListItemIcon>{page.icon}</ListItemIcon>
+        <ListItemIcon>{_page.icon}</ListItemIcon>
         <ListItemText
-          primary={page ? page.title : ""}
-          secondary={page ? page.subtitle : ""}
+          primary={_page ? _page.title : ""}
+          secondary={_page ? _page.subtitle : ""}
         />
         <KeyboardArrowRight />
       </ListItem>
     );
-  }
+  };
 
-  render() {
-    const { server } = this.props;
-    const { page, page_title } = this.state;
-    return (
-      <div className="layout">
-        <div className={"modalContent " + (this.state.open ? "open" : "")}>
-          <Card square className="modalContentCard">
-            {this.component}
-          </Card>
-        </div>
-        <header className="layout_header showMobile">
-          <div className="layout_header_top_bar">
-            <div
-              className={(!page ? "show " : "") + "layout_header_top_bar_title"}
-            >
-              <h2>Settings</h2>
-            </div>
-            <div
-              className={(page ? "show " : "") + "layout_header_top_bar_title"}
-              style={{ right: 80 }}
-            >
-              <IconButton onClick={() => this.history.push("/settings")}>
-                <KeyboardArrowLeft style={{ color: "white" }} />
-              </IconButton>
-              <h2 style={{ paddingLeft: 4 }}>{page_title}</h2>
-            </div>
-            <div className="showMobile">
-              <UserButton history={this.history} type="button" color="white" />
-            </div>
-          </div>
-        </header>
-
-        <div className="layout_two_columns">
-          <div
-            className={(page ? "hide " : "") + "layout_content wrapperMobile"}
-          >
-            <List
-              subheader={
-                <ListSubheader disableSticky={true}>Your account</ListSubheader>
-              }
-            >
-              {this.drawListItem(this.SETTINGS.ACCOUNTS)}
-              {this.drawListItem(this.SETTINGS.IMPORT_EXPORT)}
-              {this.drawListItem(this.SETTINGS.THEME)}
-              {this.drawListItem(this.SETTINGS.SOCIAL_NETWORKS)}
-            </List>
-
-            {server.isLogged ? (
-              <List
-                subheader={
-                  <ListSubheader disableSticky={true}>Hosting</ListSubheader>
-                }
-              >
-                {this.drawListItem(this.SETTINGS.PROFILE)}
-                {this.drawListItem(this.SETTINGS.SERVER)}
-                {this.drawListItem(this.SETTINGS.SECURITY)}
-                {server.saas
-                  ? this.drawListItem(this.SETTINGS.SUBSCRIPTION)
-                  : ""}
-                <Link to="/logout">
-                  <ListItem button>
-                    <ListItemIcon>
-                      <PowerSettingsNewIcon />
-                    </ListItemIcon>
-                    <ListItemText
-                      primary="Logout"
-                      secondary={`Disconnect from ${server.name}`}
-                    />
-                  </ListItem>
-                </Link>
-              </List>
-            ) : (
-              ""
-            )}
-
-            <List
-              subheader={
-                <ListSubheader disableSticky={true}>
-                  More settings
-                </ListSubheader>
-              }
-            >
-              {this.drawListItem(this.SETTINGS.APP)}
-              {this.drawListItem(this.SETTINGS.HELP)}
-            </List>
-          </div>
-
-          {page ? <div className="layout_noscroll">{page.component}</div> : ""}
-        </div>
+  return (
+    <div className="layout">
+      <div className={"modalContent " + (open ? "open" : "")}>
+        <Card square className="modalContentCard">
+          {component}
+        </Card>
       </div>
-    );
-  }
+      <header className="layout_header showMobile">
+        <div className="layout_header_top_bar">
+          <div
+            className={(!page ? "show " : "") + "layout_header_top_bar_title"}
+          >
+            <h2>Settings</h2>
+          </div>
+          <div
+            className={(page ? "show " : "") + "layout_header_top_bar_title"}
+            style={{ right: 80 }}
+          >
+            <IconButton
+              onClick={() => {
+                setPage(null);
+                history.push("/settings");
+              }}
+            >
+              <KeyboardArrowLeft style={{ color: "white" }} />
+            </IconButton>
+            <h2 style={{ paddingLeft: 4 }}>{pageTitle}</h2>
+          </div>
+          <div className="showMobile">
+            <UserButton history={history} type="button" color="white" />
+          </div>
+        </div>
+      </header>
+
+      <div className="layout_two_columns">
+        <div className={(page ? "hide " : "") + "layout_content wrapperMobile"}>
+          <List
+            subheader={
+              <ListSubheader disableSticky={true}>Your account</ListSubheader>
+            }
+          >
+            {drawListItem(SETTINGS.ACCOUNTS)}
+            {drawListItem(SETTINGS.IMPORT_EXPORT)}
+            {drawListItem(SETTINGS.THEME)}
+            {drawListItem(SETTINGS.SOCIAL_NETWORKS)}
+          </List>
+
+          {server.isLogged ? (
+            <List
+              subheader={
+                <ListSubheader disableSticky={true}>Hosting</ListSubheader>
+              }
+            >
+              {drawListItem(SETTINGS.PROFILE)}
+              {drawListItem(SETTINGS.SERVER)}
+              {drawListItem(SETTINGS.SECURITY)}
+              {server.saas ? drawListItem(SETTINGS.SUBSCRIPTION) : ""}
+              <Link to="/logout">
+                <ListItem button>
+                  <ListItemIcon>
+                    <PowerSettingsNewIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Logout"
+                    secondary={`Disconnect from ${server.name}`}
+                  />
+                </ListItem>
+              </Link>
+            </List>
+          ) : (
+            ""
+          )}
+
+          <List
+            subheader={
+              <ListSubheader disableSticky={true}>More settings</ListSubheader>
+            }
+          >
+            {drawListItem(SETTINGS.APP)}
+            {drawListItem(SETTINGS.HELP)}
+          </List>
+        </div>
+
+        {page ? <div className="layout_noscroll">{page.component}</div> : ""}
+      </div>
+    </div>
+  );
 }
-
-Settings.propTypes = {
-  server: PropTypes.object.isRequired
-};
-
-const mapStateToProps = (state, ownProps) => {
-  return { server: state.server };
-};
-
-export default connect(mapStateToProps)(Settings);
