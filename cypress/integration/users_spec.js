d8563592c48b6245b48a6a5f351e75bddd32915a
Sébastien Barbier <contact@sebastienbarbier.com>
2020-05-23 15:50:13 +0200

    Delete user on cypress user test

--- cypress/integration/users_spec.js
+++ cypress/integration/users_spec.js
@@ -30,9 +30,9 @@ describe("Users", () => {
       ".open > .welcoming__layout > .content > form > .MuiButtonBase-root"
     ).click();
 
-    cy.get(".content > .MuiButton-text > .MuiButton-label > .text").contains(
-      "test.seven23.io"
-    );
+    cy.get(".content > .MuiButton-text > .MuiButton-label > .text")
+      .should("be.visible")
+      .contains("test.seven23.io");
 
     // signup
     cy.get(".spaceBetween > div > :nth-child(2)").click();
@@ -84,68 +84,25 @@ describe("Users", () => {
     ).click();
 
     // Pay
-    cy.get(".right > .wrapperMobile > .MuiButtonBase-root").click();
-    cy.get(
-      '[style="padding: 0px; margin: 0px;"] > a > .MuiButtonBase-root'
-    ).click();
-    cy.get(".layout_content > :nth-child(2) > :nth-child(5)").click();
-    cy.wait(1000);
-    cy.get(".layout_content > :nth-child(2) > :nth-child(4)").click();
-    cy.wait(500);
-    cy.get(".layout_content > :nth-child(2) > :nth-child(5)").click();
-    cy.get("#customButton").click();
-    cy.wait(4000);
+    // cy.get(".right > .wrapperMobile > .MuiButtonBase-root").click();
+    // cy.get(
+    //   '[style="padding: 0px; margin: 0px;"] > a > .MuiButtonBase-root'
+    // ).click();
+    // cy.get(".layout_content > :nth-child(2) > :nth-child(5)").click();
+    // cy.get('#customButton').click()
 
-    // Working with iframes
-    // https://www.cypress.io/blog/2020/02/12/working-with-iframes-in-cypress/
-    //
-    // cy.get('.stripe_checkout_app').then($iframe => {
-    //   console.log($iframe[$iframe.length-1])
-    //   console.log($iframe[$iframe.length-1].contentWindow)
-    //   console.log($iframe[$iframe.length-1].contentWindow.document)
-    //   var iframe = $iframe[$iframe.length-1]
-    //   const doc = $iframe[$iframe.length-1].contentWindow.document
-    //   console.log(doc)
-    //   let input = doc.find('input')[0]
-    //   cy
-    //     .wrap(input)
-    //     .type(user.email)
-    //   input = doc.find('input')[1]
-    //   // super weird stuff here, if you just input '4242424242424242', the value
-    //   // that you end up seing in the input element is jumbled up a little,
-    //   // probably because of the way how Stripe inserts spaces while you are
-    //   // typing. By luck I found out that this issue can get worked around if
-    //   // you just chain-call type()
-    //   cy
-    //     .wrap(input)
-    //     .type('4242')
-    //     .type('4242')
-    //     .type('4242')
-    //     .type('4242')
-    //   input = doc.find('input')[2]
-    //   cy
-    //     .wrap(input)
-    //     .clear()
-    //     .type('12')
-    //     .type('20')
-    //   input = doc.find('input')[3]
-    //   cy
-    //     .wrap(input)
-    //     .type('123')
-    //     .type('{enter}')
-    // })
     // Delete account
-    // cy.get('.layout_content > :nth-child(2) > :nth-child(2)').click()
-    // cy.get('.layout_noscroll > .MuiList-root > :nth-child(7)').click()
-    // cy.get('.MuiInputBase-input').type('wrong password')
-    // cy.get('.MuiButton-contained').click()
-    // cy.get('.MuiFormHelperText-root').contains('Password incorrect')
-    // cy.get('.MuiInputBase-input').type(user.password)
-    // cy.get('.MuiButton-contained').click()
+    cy.get(".layout_content > :nth-child(2) > :nth-child(2)").click();
+    cy.get(".layout_noscroll > .MuiList-root > :nth-child(7)").click();
+    cy.get(".MuiInputBase-input").type("wrong password");
+    cy.get(".MuiButton-contained").click();
+    cy.get(".MuiFormHelperText-root").contains("Password incorrect");
+    cy.get(".MuiInputBase-input").type(user.password);
+    cy.get(".MuiButton-contained").click();
 
-    // // Back to welcome page
-    // cy.get(".open > .welcoming__layout > header > h2").contains(
-    // "Welcome on board"
-    // );
+    // Back to welcome page
+    cy.get(".open > .welcoming__layout > header > h2").contains(
+      "Welcome on board"
+    );
   });
 });
