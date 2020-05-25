8253cbc5facdb8fe86138656b298563d421b769d
Sébastien Barbier <contact@sebastienbarbier.com>
2020-05-25 22:40:11 +0200

    Fix logout test on cypress

--- cypress/integration/user_logout.js
+++ cypress/integration/user_logout.js
@@ -21,11 +21,13 @@ describe("User logout", () => {
     cy.get(".open > .welcoming__layout > header > h2").should("be.visible");
     cy.login(user);
     cy.get(".welcoming__wrapper").should("not.be.visible");
+
+    cy.get(".right > .wrapperMobile > .MuiButtonBase-root").click();
     cy.get(".MuiPaper-root > :nth-child(1) > .MuiButtonBase-root").should(
       "not.have.class",
       "Mui-disabled"
     );
-
+    cy.get("#user-popper").click();
     cy.createTransaction({
       label: "Transaction 2",
       price: -11.4,
