5b860d2c9ac6fc5c17b92276f15632922ca53237
Sébastien Barbier <contact@sebastienbarbier.com>
2020-05-28 06:39:46 +0200

    Implement a test for #34

---
+++ cypress/integration/user_login.js
@@ -0,0 +1,45 @@
+import moment from "moment";
+
+describe("User login", () => {
+  const user = {
+    username: `test${moment().format("YYYYMMDDHHmmss")}`,
+    email: `test${moment().format("YYYYMMDDHHmmss")}@seven23.io`,
+    password: "qwerty",
+  };
+
+  const account = {
+    name: "test Account",
+    currency: "Euro",
+  };
+
+  before(function () {
+    cy.createUserWithApi(user, account);
+  });
+
+  it("should login if a local account exist", () => {
+    cy.logout();
+    cy.setLocalAccount();
+    cy.get(".right > .wrapperMobile > .MuiButtonBase-root").click();
+    cy.get('[style="padding: 0px; margin: 0px;"] > :nth-child(5)').click();
+    cy.get(".open > .welcoming__layout > header > h2").should("be.visible");
+    // Login form
+    cy.get(
+      ".open > .welcoming__layout > .content > form > :nth-child(1) > .MuiInputBase-root > .MuiInputBase-input"
+    ).type(user.username);
+    cy.get(":nth-child(3) > .MuiInputBase-root > .MuiInputBase-input").type(
+      user.password
+    );
+    cy.get(
+      ".open > .welcoming__layout > .content > form > .MuiButtonBase-root"
+    ).click();
+    cy.get(".open > .welcoming__layout > header > h2").should("not.be.visible");
+
+    cy.get(
+      '[aria-hidden="false"] > .MuiPaper-root > div.balance > p > [style="color: rgb(67, 144, 244);"] > .amount > .balance'
+    ).should("be.visible");
+  });
+
+  after(() => {
+    cy.deleteUserWithApi(user);
+  });
+});
