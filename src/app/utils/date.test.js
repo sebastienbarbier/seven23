f973640754959c7d2b1ee804e23befc8ee2dddb8
Sébastien Barbier <contact@sebastienbarbier.com>
2020-08-26 22:45:19 +0200

    Add tests for dateToString and stringToDate

--- src/app/utils/date.test.js
+++ src/app/utils/date.test.js
@@ -1,8 +1,22 @@
-import { isLeapYear } from "./date";
+import { isLeapYear, dateToString, stringToDate } from "./date";
 
 test("isLeapYear", () => {
   expect(isLeapYear(2017)).toBe(false);
   expect(isLeapYear(2018)).toBe(false);
   expect(isLeapYear(2019)).toBe(false);
   expect(isLeapYear(2020)).toBe(true);
+});
+
+test("dateToString", () => {
+  expect(dateToString(new Date(2020, 0, 31))).toBe(`2020-01-31`);
+  expect(dateToString(new Date(2020, 1, 29))).toBe(`2020-02-29`);
+  expect(dateToString(new Date(2020, 11, 31))).toBe(`2020-12-31`);
+});
+
+test("stringToDate", () => {
+  const date = stringToDate(`2020-01-31`);
+  expect(date instanceof Date).toBe(true);
+  expect(date.getFullYear()).toBe(2020);
+  expect(date.getMonth()).toBe(0);
+  expect(date.getDate()).toBe(31);
 });
