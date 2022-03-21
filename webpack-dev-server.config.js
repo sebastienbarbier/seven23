4e6849f0e3f8dfc170ce8dc803d2bd07df7c4a96
Sébastien Barbier <contact@sebastienbarbier.com>
2022-03-21 14:37:58 +0100

    Fix missing env in process for dev environement

--- webpack-dev-server.config.js
+++ webpack-dev-server.config.js
@@ -29,7 +29,7 @@ const config = {
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
-      "process": {
+      "process.env": {
       },
     }),
     // Enables Hot Modules Replacement
