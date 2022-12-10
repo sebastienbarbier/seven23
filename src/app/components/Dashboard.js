d809bb1d432a98a9712fbbbc09c69b16c7d7a0f8
Sébastien Barbier <contact@sebastienbarbier.com>
2022-12-10 22:13:41 +0700

    Refactor Dashboard, migrate resetPAssword to new UI

--- src/app/components/Dashboard.js
+++ src/app/components/Dashboard.js
@@ -37,24 +37,91 @@ import MigrateToCloud from './alerts/MigrateToCloud';
 import { BalancedAmount, ColoredAmount } from "./currency/Amount";
 
 export default function Dashboard(props) {
+
   const theme = useTheme();
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  // Fetch visibility status of numbers
+  const isConfidential = useSelector((state) => state.app.isConfidential);
+
+  // Fetch data from state
+  const transactions = useSelector((state) => state.transactions);
+  const categories = useSelector((state) =>
+    state.categories ? state.categories.list : null
+  );
+  const changes = useSelector((state) =>
+    state.changes ? state.changes.list : null
+  );
+
+  // generate stats for calendar graph based on statistics data
   const [statistics, setStatistics] = useState(null);
+  const [calendar, setCalendar] = useState(null);
+  // If transactions change, we refresh statistics
+  useEffect(() => {
+    if (!transactions) {
+      setStatistics(null);
+    } else {
+      const now = moment().endOf('month').toDate();
+      const from = moment(now).subtract(4, 'months').toDate();
+      dispatch(StatisticsActions.dashboard(from, now))
+        .then((result) => {
+
+          const calendar = result.stats.calendar.filter(day => {
+            return day.date <= now && day.date >= from ;
+          });
+
+          result.graph[0].color = theme.palette.numbers.red;
+          result.graph[1].color = theme.palette.numbers.blue;
+          setOpenTrend(false);
+          setCalendar(calendar);
+          setStatistics(result);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }, [transactions]);
+
+
+  const valid_until = useSelector((state) => state.user?.profile?.valid_until);
+
+  // Alert states
+  const show_update_alert = useSelector((state) => state.state.cacheDidUpdate);
+  const show_migration_alert = useSelector((state) =>
+    state.server.isLogged && state.accounts.remote.length === 0
+  );
+  const [show_expiring_soon_alert, set_show_expiring_soon_alert] = useState(false);
+  const [show_expired_alert, set_show_expired_alert] = useState(false);
+
+  // When valid until change we check if user needs to be alerted about his subscription
+  useEffect(() => {
+    if (valid_until) {
+      if (new Date(valid_until) < new Date()) {
+        set_show_expiring_soon_alert(false);
+        set_show_expired_alert(true);
+      } else if (moment(valid_until).diff(new Date(), 'days') < 7) {
+        set_show_expiring_soon_alert(true);
+        set_show_expired_alert(false);
+      }
+    } else {
+      set_show_expiring_soon_alert(false);
+      set_show_expired_alert(false);
+    }
+  }, [valid_until])
+
+  // Toggle Trend component as custom modal view
   const [openTrend, setOpenTrend] = useState(false);
+  const [trendComponent, setTrendComponent] = useState(null);
+  const handleToggleTrend = (trend) => {
+    setTrendComponent(trend && trend.component ? trend.component : null);
+    setOpenTrend(!openTrend);
+  };
+
+  // Handle SwipeableViews (enable/disable based on window.innerWidth)
   const [disableSwipeableViews, setDisableSwipeableViews] = useState(
     () => window.innerWidth > 600
   );
-  const [trendComponent, setTrendComponent] = useState(null);
-  const selectedCurrency = useSelector((state) =>
-    state.account
-      ? state.currencies.find((c) => c.id === state.account.currency)
-      : null
-  );
-
-  const isConfidential = useSelector((state) => state.app.isConfidential);
-  const cacheDidUpdate = useSelector((state) => state.state.cacheDidUpdate);
-
   useEffect(() => {
     function checkWidth() {
       setDisableSwipeableViews(window.innerWidth > 600);
@@ -63,81 +130,7 @@ export default function Dashboard(props) {
     return () => {
       window.removeEventListener("resize", checkWidth);
     };
-  });
-
-  const changes = useSelector((state) =>
-    state.changes ? state.changes.list : null
-  );
-  const categories = useSelector((state) =>
-    state.categories ? state.categories.list : null
-  );
-  const transactions = useSelector((state) => state.transactions);
-
-  // generate stats for calendar graph based on statistics data
-  const [calendar, setCalendar] = useState(null);
-  // If transactions change, we refresh statistics
-  useEffect(() => {
-    if (!props.loadingOnly) {
-      if (!transactions) {
-        setStatistics(null);
-      } else {
-        const now = moment().endOf('month').toDate();
-        const from = moment(now).subtract(4, 'months').toDate();
-        dispatch(StatisticsActions.dashboard(from, now))
-          .then((result) => {
-
-            const calendar = result.stats.calendar.filter(day => {
-              return day.date <= now && day.date >= from ;
-            });
-
-            result.graph[0].color = theme.palette.numbers.red;
-            result.graph[1].color = theme.palette.numbers.blue;
-            setOpenTrend(false);
-            setCalendar(calendar);
-            setStatistics(result);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      }
-    }
-  }, [transactions]);
-
-  const handleToggleTrend = (trend) => {
-    setTrendComponent(trend && trend.component ? trend.component : null);
-    setOpenTrend(!openTrend);
-  };
-
-
-  const valid_until = useSelector((state) => state.user?.profile?.valid_until);
-
-  const [subscription_expire_soon, set_subscription_expire_soon] = useState(false);
-  const [subscription_has_expire, set_subscription_has_expire] = useState(false);
-  const [valid_until_moment, set_valid_until_moment] = useState(null);
-
-  const should_migrate_account = useSelector((state) =>
-    state.server.isLogged && state.accounts.remote.length === 0
-  );
-
-  useEffect(() => {
-    if (valid_until) {
-
-      const date_moment = moment(valid_until);
-      set_valid_until_moment(date_moment);
-
-      const valid_until_date = new Date(valid_until);
-      if (valid_until_date < new Date()) {
-        set_subscription_has_expire(true);
-        set_subscription_expire_soon(false);
-      } else if (date_moment.diff(new Date(), 'days') < 7) {
-        set_subscription_expire_soon(true);
-        set_subscription_has_expire(false);
-      }
-    } else {
-      set_subscription_expire_soon(false);
-      set_subscription_has_expire(false);
-    }
-  }, [valid_until])
+  }, []);
 
   return (
     <div className="layout dashboard">
@@ -235,7 +228,7 @@ export default function Dashboard(props) {
                 <p>
                   This account contains{" "}
                   <span style={{ color: theme.palette.transactions.main }}>
-                    {!transactions || props.loadingOnly ? (
+                    {!transactions ? (
                       <span className="loading w80" />
                     ) : (
                       transactions.length
@@ -243,7 +236,7 @@ export default function Dashboard(props) {
                   </span>{" "}
                   <strong>transactions</strong>,{" "}
                   <span style={{ color: theme.palette.changes.main }}>
-                    {!changes || props.loadingOnly ? (
+                    {!changes ? (
                       <span className="loading w80" />
                     ) : (
                       changes.length
@@ -251,7 +244,7 @@ export default function Dashboard(props) {
                   </span>{" "}
                   <strong>changes</strong>, and{" "}
                   <span style={{ color: theme.palette.categories.main }}>
-                    {!categories || props.loadingOnly ? (
+                    {!categories ? (
                       <span className="loading w80" />
                     ) : (
                       categories.length
@@ -266,16 +259,16 @@ export default function Dashboard(props) {
               <Stack spacing={2}>
 
                 {/* App has updated and need to restart */}
-                { cacheDidUpdate && <NewVersionAvailable /> }
+                { show_update_alert && <NewVersionAvailable /> }
 
                 {/* Expiration date is coming soon */}
-                { subscription_expire_soon && <SubscriptionExpireSoon valid_until_moment={valid_until_moment} /> }
+                { show_expiring_soon_alert && <SubscriptionExpireSoon valid_until_moment={valid_until ? moment(valid_until) : null} /> }
 
                 {/* Expiration date is passed */}
-                { subscription_has_expire && <SubscriptionExpired /> }
+                { show_expired_alert && <SubscriptionExpired /> }
 
                 {/* User is logged with local account but no account on server */}
-                { should_migrate_account && <MigrateToCloud /> }
+                { show_migration_alert && <MigrateToCloud /> }
 
               </Stack>
             </div>
