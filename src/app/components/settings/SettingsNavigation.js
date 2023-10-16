fc3548b4c8c295f5f94c089cca88faeb8e824ec5
Sébastien Barbier <contact@sebastienbarbier.com>
2023-10-16 19:08:19 +0700

    Remove console.log

--- src/app/components/settings/SettingsNavigation.js
+++ src/app/components/settings/SettingsNavigation.js
@@ -197,8 +197,6 @@ export default function SettingsNavigation(props) {
       });
   };
 
-  console.log(props.hideMobile);
-
   return (
       <div className={"layout_content wrapperMobile mobile_footer_padding" + (props.hideMobile ? ' hideMobile' : '') + (props.showMobile ? ' showMobile' : '')}>
         <List
